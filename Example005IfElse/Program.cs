14ab27b6ecc1e7c8e50cfc38f975c456429e98da
iryna <rinakinsha@yndex.by>
2022-06-17 00:08:23 +0300

    Lili

---
+++ Example005IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лили")
+{
+    Console.WriteLine("После стольких лет? Всегда...");
+}
+else
+{
+   Console.Write("Да здавствует ");
+   Console.Write(username);
+}
