05576357c1f6d9800452b288263380b3c9be5b3b
iryna <rinakinsha@yndex.by>
2022-06-23 11:27:35 +0300

    Error correction 005

--- Example005IfElse/Program.cs
+++ Example005IfElse/Program.cs
@@ -1,12 +1,20 @@
 ﻿Console.Write("Введите Ваше имя: ");
-string username = Console.ReadLine();
 
-if(username.ToLower() == "лили")
+string? username = Console.ReadLine();
+
+if (!String.IsNullOrWhiteSpace(username))
+{
+ if (username.ToLower() == "лили")
 {
     Console.WriteLine("После стольких лет? Всегда...");
 }
-else
+ else
 {
-   Console.Write("Да здавствует ");
+   Console.Write("Да здравствует ");
    Console.Write(username);
+}
+}
+else 
+{
+    Console.Write("Пустая строка");
 }
