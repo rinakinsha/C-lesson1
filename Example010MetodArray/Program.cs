29a2052694ef04ee71e37c89b44e76bc3f199aee
iryna <rinakinsha@yndex.by>
2022-06-23 12:08:09 +0300

    Example010 Metod Array

---
+++ Example010MetodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 11, 21, 38, 74, 57, 86, 47, 11, 8 };
+
+int n = array.Length;
+int find = 11;
+
+int index = 0;
+
+while (index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+   index++;
+}
